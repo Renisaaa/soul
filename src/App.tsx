a03536ff63a2e6c4859d7dffbd8ca6f250c2e3dd
Renisa <renisabhardwaj@gmail.com>
2026-01-17 13:47:37 +0530

    undeployed website

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import SpiritualWellnessServices from './components/SpiritualWellnessServices';
 
 function App() {
-  return <SpiritualWellnessServices />;
+  return <div></div>;
 }
 
 export default App;
